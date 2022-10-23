edacfe3d286d0fc042f8460024c2c7ee6f8e70c2
Diego Jonguitud DellPc <djonguitud95@gmail.com>
2022-10-23 16:44:57 -0500

    merge: query-cities-feat to main

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,9 @@ const apiKey = '0caa83d5194f964f7490af6e5c39d537';
 const elInputUser = $('#input');
 const elLocalStorage = $('.local-storage');
 const elNextDays = $('.next-days__wraper');
-const elBtn = $('.btn');
-let city;
-const queryURL = 'https://api.openweathermap.org/data/2.5/weather?' + city + '&appid=' + apiKey;
+const elQueryForm = $('#form-query');
+/* const queryURL = 'https://api.openweathermap.org/data/2.5/weather?' + city + '&appid=' + apiKey; */
+const citiesQueue = [];
 
 /*=====  End of Section comment block  ======*/
 
@@ -21,18 +21,66 @@ const queryURL = 'https://api.openweathermap.org/data/2.5/weather?' + city + '&a
 =            EVENTS SECTION           =
 =============================================*/
 
-elBtn.on(click, () => {});
-
 /*=====  End of Section comment block  ======*/
 
 /*=============================================
 =            FUNCTIONS SECTION           =
 =============================================*/
+elQueryForm.submit(citySearch);
+
+//! Input task send to local storage
+function citySearch(event) {
+	event.preventDefault();
+	let issuedCity = firstUpperCase(this[0].value);
+
+	if (issuedCity !== '') {
+		console.log(issuedCity);
+		sendLocalStorage(issuedCity);
+	} else {
+		return;
+	}
+}
+
+//!Local storage
+function sendLocalStorage(city) {
+	if (citiesQueue.includes(city)) {
+		return;
+	} else {
+		citiesQueue.push(city);
+		localStorage.setItem('citiesQueue', JSON.stringify(citiesQueue));
+	}
+}
+
+//!Create buttons from local storage
+function retrieveInfoLocalStorage() {
+	let comeBack = localStorage.getItem('citiesQueue');
+	let parsed = JSON.parse(comeBack);
+	parsed.forEach((index) => {
+		elLocalStorage.append('<button type="button" class="local-btn btn btn-warning w-100">' + index + '</button>');
+	});
+}
+
+retrieveInfoLocalStorage();
 
 /*=====  End of Section comment block  ======*/
 
 /*=============================================
 =            MISC SECTION           =
 =============================================*/
+
+//!Convert first letter to Upper Case
+function firstUpperCase(word) {
+	switch (word) {
+		case word.toUpperCase():
+			return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+			break;
+		case word.toLowerCase():
+			return word.charAt(0).toUpperCase() + word.slice(1);
+			break;
+		default:
+			return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+			break;
+	}
+}
 
 /*=====  End of Section comment block  ======*/
