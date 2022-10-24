bb033ba4921d0eb3a3a3a0caea4acffe91c90c16
Diego Jonguitud DellPc <djonguitud95@gmail.com>
2022-10-23 20:34:18 -0500

    chore: add id'sto render weather info for each day
    feat: retrieve weather info from open weather app to render on html cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,15 @@
 /*=============================================
 =            VARIABLES  SECTION           =
 =============================================*/
-
-//!API  INFO
-//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
-const apiKey = '0caa83d5194f964f7490af6e5c39d537';
+const apiKey = '021da88feaf1b9241beccb1c30ea00c4&units=imperial';
 const elInputUser = $('#input');
 const elLocalStorage = $('.local-storage');
 const elNextDays = $('.next-days__wraper');
 const elQueryForm = $('#form-query');
-/* const queryURL = 'https://api.openweathermap.org/data/2.5/weather?' + city + '&appid=' + apiKey; */
+let elToday = $('#today-date');
+let currentDay = moment().format('MMM Do YY');
+let city;
+
 const citiesQueue = [];
 
 /*=====  End of Section comment block  ======*/
@@ -20,13 +19,37 @@ const citiesQueue = [];
 /*=============================================
 =            EVENTS SECTION           =
 =============================================*/
+elQueryForm.submit(citySearch);
 
 /*=====  End of Section comment block  ======*/
 
 /*=============================================
 =            FUNCTIONS SECTION           =
 =============================================*/
-elQueryForm.submit(citySearch);
+//!Fetch today's weather from API Open Weather
+function queryTodayWeather(city) {
+	let queryURLWeather = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+	fetch(queryURLWeather)
+		.then((resp1) => resp1.json())
+		.then((data) => {
+			todaysWeather(data.main.temp, data.wind.speed, data.main.humidity);
+			iconRender(data);
+		});
+}
+
+//!Fetch forecast weather from API Open Weather
+function queryURLForecast(city) {
+	let queryURLForecast = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey;
+	fetch(queryURLForecast)
+		.then((resp2) => resp2.json())
+		.then((data) => {
+			renderday1(data.list[4].dt_txt, data.list[4].main.temp, data.list[4].wind.speed, data.list[4].main.humidity, data.list[4].weather[0].icon);
+			renderday2(data.list[11].dt_txt, data.list[11].main.temp, data.list[11].wind.speed, data.list[11].main.humidity, data.list[11].weather[0].icon);
+			renderday3(data.list[19].dt_txt, data.list[19].main.temp, data.list[19].wind.speed, data.list[19].main.humidity, data.list[19].weather[0].icon);
+			renderday4(data.list[25].dt_txt, data.list[25].main.temp, data.list[25].wind.speed, data.list[25].main.humidity, data.list[25].weather[0].icon);
+			renderday5(data.list[35].dt_txt, data.list[35].main.temp, data.list[35].wind.speed, data.list[35].main.humidity, data.list[35].weather[0].icon);
+		});
+}
 
 //! Input task send to local storage
 function citySearch(event) {
@@ -36,6 +59,9 @@ function citySearch(event) {
 	if (issuedCity !== '') {
 		console.log(issuedCity);
 		sendLocalStorage(issuedCity);
+		queryTodayWeather(issuedCity);
+		queryURLForecast(issuedCity);
+		$('#today-city').html(issuedCity + " <span id='today-date' class='text-success h5'>" + '(' + currentDay + ')' + '</span>');
 	} else {
 		return;
 	}
@@ -58,6 +84,65 @@ function retrieveInfoLocalStorage() {
 	parsed.forEach((index) => {
 		elLocalStorage.append('<button type="button" class="local-btn btn btn-warning w-100">' + index + '</button>');
 	});
+}
+
+//! Render openweather on today's weather card
+function todaysWeather(temp, wind, humidity) {
+	$('#today-temp').text('Temp: ' + temp + ' ℉');
+	$('#today-wind').text('Wind Speed: ' + wind + ' km/h');
+	$('#today-humidity').text('Humidity: ' + humidity + ' %');
+}
+
+//!Render forecast day1
+function renderday1(date, temp, wind, humidity, icon) {
+	$('#dayOne-date').text(date);
+	$('#dayOne-temp').text('Temp: ' + temp + ' ℉');
+	$('#dayOne-wind').text('Wind Speed: ' + wind + ' km/h');
+	$('#dayOne-humidity').text('Humidity: ' + humidity + ' %');
+	$('#dayOne-icon').attr('src', 'http://openweathermap.org/img/w/' + String(icon) + '.png');
+}
+
+//!Render forecast day2
+function renderday2(date, temp, wind, humidity, icon) {
+	$('#dayTwo-date').text(date);
+	$('#dayTwo-temp').text('Temp: ' + temp + ' ℉');
+	$('#dayTwo-wind').text('Wind Speed: ' + wind + ' km/h');
+	$('#dayTwo-humidity').text('Humidity: ' + humidity + ' %');
+	$('#dayTwo-icon').attr('src', 'http://openweathermap.org/img/w/' + String(icon) + '.png');
+}
+
+//!Render forecast day3
+function renderday3(date, temp, wind, humidity, icon) {
+	$('#dayThree-date').text(date);
+	$('#dayThree-temp').text('Temp: ' + temp + ' ℉');
+	$('#dayThree-wind').text('Wind Speed: ' + wind + ' km/h');
+	$('#dayThree-humidity').text('Humidity: ' + humidity + ' %');
+	$('#dayThree-icon').attr('src', 'http://openweathermap.org/img/w/' + String(icon) + '.png');
+}
+
+//!Render forecast day4
+function renderday4(date, temp, wind, humidity, icon) {
+	$('#dayFour-date').text(date);
+	$('#dayFour-temp').text('Temp: ' + temp + ' ℉');
+	$('#dayFour-wind').text('Wind Speed: ' + wind + ' km/h');
+	$('#dayFour-humidity').text('Humidity: ' + humidity + ' %');
+	$('#dayFour-icon').attr('src', 'http://openweathermap.org/img/w/' + String(icon) + '.png');
+}
+
+//!Render forecast day5
+function renderday5(date, temp, wind, humidity, icon) {
+	$('#dayFive-date').text(date);
+	$('#dayFive-temp').text('Temp: ' + temp + ' ℉');
+	$('#dayFive-wind').text('Wind Speed: ' + wind + ' km/h');
+	$('#dayFive-humidity').text('Humidity: ' + humidity + ' %');
+	$('#dayFive-icon').attr('src', 'http://openweathermap.org/img/w/' + String(icon) + '.png');
+}
+
+//!Render weather icon
+function iconRender(i) {
+	let iconcode = i.weather[0].icon;
+	let iconurl = 'http://openweathermap.org/img/w/' + iconcode + '.png';
+	$('#today-icon').attr('src', iconurl);
 }
 
 retrieveInfoLocalStorage();
